edc18c82d178b41267bd1f680ad5d247d6e88233
genaire <genaire@engineer.com>
2024-06-16 22:05:24 +0100

    Add files via upload

---
+++ generation/generate_validator_proof.go
@@ -0,0 +1,74 @@
+package generation
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"os"
+
+	eigenpodproofs "github.com/Layr-Labs/eigenpod-proofs-generation"
+	beacon "github.com/Layr-Labs/eigenpod-proofs-generation/beacon"
+	"github.com/attestantio/go-eth2-client/spec/deneb"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/rs/zerolog/log"
+)
+
+func GenerateValidatorFieldsProof(oracleBlockHeaderFile string, stateFile string, validatorIndex uint64, chainID uint64, output string) error {
+
+	var state deneb.BeaconState
+	var oracleBeaconBlockHeader phase0.BeaconBlockHeader
+	stateJSON, err := ParseDenebStateJSONFile(stateFile)
+	if err != nil {
+		log.Debug().Msg("GenerateValidatorFieldsProof: error with JSON parsing")
+		return err
+	}
+	ParseDenebBeaconStateFromJSON(*stateJSON, &state)
+
+	oracleBeaconBlockHeader, err = ExtractBlockHeader(oracleBlockHeaderFile)
+	if err != nil {
+		log.Debug().AnErr("Error with parsing header file", err)
+		return err
+	}
+
+	beaconStateRoot, err := state.HashTreeRoot()
+
+	if err != nil {
+		log.Debug().AnErr("Error with HashTreeRoot of state", err)
+		return err
+	}
+
+	epp, err := eigenpodproofs.NewEigenPodProofs(chainID, 1000)
+	if err != nil {
+		log.Debug().AnErr("Error creating EPP object", err)
+		return err
+	}
+
+	versionedState, err := beacon.CreateVersionedState(&state)
+	if err != nil {
+		log.Debug().AnErr("Error with CreateVersionedState", err)
+		return err
+	}
+
+	stateRootProof, validatorFieldsProof, err := ProveValidatorFields(epp, &oracleBeaconBlockHeader, &versionedState, uint64(validatorIndex))
+	if err != nil {
+		log.Debug().AnErr("Error with ProveValidatorFields", err)
+		return err
+	}
+
+	proofs := WithdrawalCredentialProofs{
+		StateRootAgainstLatestBlockHeaderProof: ConvertBytesToStrings(stateRootProof.StateRootProof),
+		BeaconStateRoot:                        "0x" + hex.EncodeToString(beaconStateRoot[:]),
+		ValidatorIndex:                         uint64(validatorIndex),
+		WithdrawalCredentialProof:              ConvertBytesToStrings(validatorFieldsProof),
+		ValidatorFields:                        GetValidatorFields(state.Validators[validatorIndex]),
+	}
+
+	proofData, err := json.Marshal(proofs)
+	if err != nil {
+		log.Debug().AnErr("JSON marshal error: ", err)
+		return err
+	}
+
+	_ = os.WriteFile(output, proofData, 0644)
+
+	return nil
+}
